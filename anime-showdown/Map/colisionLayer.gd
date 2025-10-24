395bd0f22e790b65c48330f3c5f9c676a2b3a27d
Quinn Bancescu <quinn.bancescu@gmail.com>
2025-10-24 11:46:54 -0600

    fix colisions

--- anime-showdown/Map/colisionLayer.gd
+++ anime-showdown/Map/colisionLayer.gd
@@ -1,10 +1,18 @@
 extends TileMapLayer
 
-func _ready() -> void:
-	var tiles = get_used_cells()
-	for tile: Vector2i in tiles:
+#func _ready() -> void:
+	#var tiles = get_used_cells()
+	#for tile: Vector2i in tiles:
+		#var aroundTiles := get_surrounding_cells(tile)
+		#for sourondingTile: Vector2i in aroundTiles:
+			#if get_cell_source_id(sourondingTile) == -1:
+				#set_cell(sourondingTile,0,Vector2i.ZERO)
+
+func placeBoundary(tiles: Array) -> void:
+	var ourTiles = get_used_cells()
+	for tile: Vector2i in ourTiles:
 		var aroundTiles := get_surrounding_cells(tile)
 		for sourondingTile: Vector2i in aroundTiles:
-			if get_cell_source_id(sourondingTile) == -1:
-				set_cell(sourondingTile,0,Vector2i.ZERO)
-		
+			if tiles.find(sourondingTile) == -1:
+				if get_cell_source_id(sourondingTile) == -1:
+					set_cell(sourondingTile,0,Vector2i.ZERO)
