002aae5afdf1185e50129c9372c8da4b0a4d5833
Quinn Bancescu <quinn.bancescu@gmail.com>
2025-10-24 10:17:22 -0600

    wip

---
+++ anime-showdown/Player/player.gd
@@ -0,0 +1,9 @@
+extends CharacterBody2D
+
+func _physics_process(delta: float) -> void:
+	var direction = Input.get_vector("ui_left","ui_right","ui_up","ui_down")
+	if direction != Vector2.ZERO:
+		velocity = direction * 32
+	else:
+		velocity = Vector2.ZERO
+	move_and_slide()
