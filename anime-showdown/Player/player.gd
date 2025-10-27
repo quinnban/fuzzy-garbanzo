f8102084ab40ab2c231ba905062312585edc0d85
Quinn Bancescu <quinn.bancescu@gmail.com>
2025-10-27 13:29:50 -0600

    fix mouse position

--- anime-showdown/Player/player.gd
+++ anime-showdown/Player/player.gd
@@ -3,30 +3,32 @@ extends CharacterBody2D
 
 @onready var animated_sprite = $AnimatedSprite2D
 
-@onready var tilemap = get_parent()
-@onready var layer1 = tilemap.get_node("Layer 1")
-@onready var layer2 = tilemap.get_node("Layer 2")
-@onready var layer3 = tilemap.get_node("Layer 3")
+@onready var map = get_parent()
+@onready var layer1 = map.get_node("Layer 1")
+@onready var layer2 = map.get_node("Layer 2")
+@onready var layer3 = map.get_node("Layer 3")
+@onready var ui = map.get_node("DebugMenu")
+
 
 var last_cell: Vector2i = Vector2i(-1, -1)
 var last_tile_id: int = 3;
 
 func _process(delta: float) -> void:
-	
-	var y_offset = 64 * 0.5
-	var current_global_pos = global_position + Vector2(0, -y_offset)
+	update_mouse_debug()
+	var current_global_pos = global_position
 	var current_local_pos = layer1.to_local(current_global_pos)
+	current_local_pos.x = floor(current_local_pos.x);
+	current_local_pos.y = floor(current_local_pos.y);
 	var current_cell = layer1.local_to_map(current_local_pos)
+	ui.update_player_postion(current_cell)
+	ui.update_player_direction(last_cell)
 	if current_cell != last_cell:
 		if last_cell != Vector2i(-1, -1):
 			set_tile_at_position(last_cell,last_tile_id)
 			set_last_tile_id(last_cell)
-		set_tile_at_position(current_cell,4)
+		set_tile_at_position(current_cell,5)
 		last_cell = current_cell
-		
-		
-		
-	
+
 func set_tile_at_position(cell_pos: Vector2i, tileId: int):
 	if layer1.isInLayer(cell_pos):
 		layer1.set_cell( cell_pos, tileId, Vector2i.ZERO)
@@ -34,8 +36,7 @@ func set_tile_at_position(cell_pos: Vector2i, tileId: int):
 		layer2.set_cell( cell_pos, tileId, Vector2i.ZERO)
 	if layer3.isInLayer(cell_pos):
 		layer3.set_cell( cell_pos, tileId, Vector2i.ZERO)
-	
-	
+
 func set_last_tile_id(cell_pos: Vector2i):
 	if layer1.isInLayer(cell_pos):
 		last_tile_id = layer1.getTileId(cell_pos)
@@ -43,8 +44,16 @@ func set_last_tile_id(cell_pos: Vector2i):
 		last_tile_id = layer2.getTileId(cell_pos)
 	if layer3.isInLayer(cell_pos):
 		last_tile_id =layer3.getTileId(cell_pos)
-	
 
+func update_mouse_debug() -> void: 
+	var mouse_position_viewport = get_global_mouse_position()
+	var mouse_local_pos = layer1.to_local(mouse_position_viewport)
+	mouse_local_pos.x = floor(mouse_local_pos.x);
+	mouse_local_pos.y = floor(mouse_local_pos.y);
+	var mouse_cell = layer1.local_to_map(mouse_local_pos);
+	ui.update_test_postion(mouse_local_pos)
+	ui.update_mouse_postion(mouse_cell)
+	
 func _ready() -> void:
 	animated_sprite.stop();
 	animated_sprite.play("default");
---
+++ anime-showdown/debug_menu.gd
@@ -0,0 +1,42 @@
+extends CanvasLayer
+class_name UI
+
+@onready var pp = $%Pp;
+@onready var mp = $%Mp;
+@onready var pd = $%Pd;
+@onready var t = $%T;
+
+var player_postion = Vector2i.ZERO;
+var player_direction = Vector2i.ZERO;
+var mouse_postion = Vector2i.ZERO;
+var test = Vector2i.ZERO;
+
+func update_player_postion(value: Vector2i) -> void:	
+	player_postion = value
+	update_player_postion_text()
+
+func update_player_direction(value: Vector2i) -> void:	
+	player_direction = value;
+	update_player_direction_text()
+
+func update_mouse_postion(value: Vector2i) -> void:	
+	mouse_postion = value;
+	update_mouse_postion_text()
+	
+func update_test_postion(value: Vector2i) -> void:	
+	test = value;
+	update_test_postion_text()
+
+	
+
+func update_player_postion_text() -> void: 
+	pp.text = "Player position: " + str(player_postion.x) + "," + str(player_postion.y);
+	
+func update_player_direction_text() -> void: 
+	pd.text = "Player Last position: " + str(player_direction.x) + "," + str(player_direction.y);
+	
+func update_mouse_postion_text() -> void: 
+	mp.text = "Mouse position: " + str(mouse_postion.x) + "," + str(mouse_postion.y);
+
+func update_test_postion_text() -> void: 
+	t.text = "Mouse test position: " + str(test.x) + "," + str(test.y);
